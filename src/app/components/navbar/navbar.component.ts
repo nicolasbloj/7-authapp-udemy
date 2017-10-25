ca18493d5a3791007e26898e1d2d373f5ebf55e6
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-25 11:38:54 -0300

    clase139 auth0 dashboard and AuthService

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,25 @@
 import { AuthService } from '../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor(private auth: AuthService) { }
-
-  ngOnInit() {
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
   }
+
 
   login() {
     this.auth.login();
   }
+
+  logout() {
+    this.auth.logout();
+  }
+
 
 }
