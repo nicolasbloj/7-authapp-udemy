930de12133c7b9ae72abb985650eed160e312ed9
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-25 10:54:13 -0300

    clase137 - routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricingComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { HomeComponent } from './components/home/home.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'pricing', component: PricingComponent },
+    { path: 'protected', component: ProtectedComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
